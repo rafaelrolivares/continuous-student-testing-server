3aa53de03a7093bb681f8cb09be7872e1f5c77cf
kerenKi <kkinberg13@gmail.com>
2019-06-19 15:49:47 +0200

    merge changes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const RawData = require('./raw_data/model')
 
+const question = require('./questions/model')
+const response = require('./responses/model')
+const student = require('./students/model')
+const test = require('./tests/model')
+
+
 
 
 const cors = require('cors')
@@ -11,7 +17,11 @@ const port = process.env.PORT || 4000
 app
   .use(cors())
   .use(bodyParser.json())
+
   .use(RawData)
+
+  .use(question, response, student, test)
+
 
 
 function onListen() {

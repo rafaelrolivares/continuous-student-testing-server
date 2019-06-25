b3c6a4113180369fe5702e743bccafbbb8431490
ANDREA VAN LIERE <andreavanliere@yahoo.com>
2019-06-25 17:28:37 +0200

    Route to get all students, all questions and total questions passed

--- evaluations/routes.js
+++ evaluations/routes.js
@@ -7,173 +7,181 @@ const Exercise = require('../exercises/model')
 
 router.get('/evaluations', (req, res, next) => {
   Evaluation
-  .findAll({attributes:[['studentId','studentId'], ['questionId','questionId']],
-    group: ['studentId','questionId'],
-    order:[['studentId', 'ASC'],['questionId', 'ASC'],],
-   })
-   .then(evaluations => {
-      const evaluationArray =  evaluations.map(evaluation => {
+    .findAll({
+      attributes: [['studentId', 'studentId'], ['questionId', 'questionId']],
+      group: ['studentId', 'questionId'],
+      order: [['studentId', 'ASC'], ['questionId', 'ASC'],],
+    })
+    .then(evaluations => {
+      const evaluationArray = evaluations.map(evaluation => {
         return Evaluation
           .findAll({
-            include: [ {model: Student},
-              {model: Question, include: [Exercise]} 
-             ],
+            include: [{ model: Student },
+            { model: Question, include: [Exercise] }
+            ],
             limit: 1,
             where: {
               studentId: evaluation.studentId,
               questionId: evaluation.questionId
             },
-            order: [ [ 'createdAt', 'DESC' ]]
+            order: [['createdAt', 'DESC']]
           })
           .then(fullEvaluation => {
             return fullEvaluation
           })
-          .catch(error => next(error)) 
-      })
-
-      Promise.all(evaluationArray)
-      .then( () => {
-         return res.send({ 
-        evaluationArray })
-      })
-      .catch(error => next(error))   
-    })
-      .catch(error => next(error))
-  })
-
-  router.get('/evaluations-by-question', (req, res, next) => {
-    Evaluation
-    .findAll({attributes:[['studentId','studentId'], ['questionId','questionId']],
-      group: ['studentId','questionId'],
-      order:[['studentId', 'ASC'],['questionId', 'ASC'],],
-     })
-     .then(evaluations => {
-        const evaluationArray =  evaluations.map(evaluation => {
-          return Evaluation
-            .findAll({
-              include: [ {model: Student},
-                {model: Question, include: [Exercise]} 
-               ],
-              limit: 1,
-              where: {
-                studentId: evaluation.studentId,
-                questionId: evaluation.questionId
-              },
-              order: [ [ 'createdAt', 'DESC' ]]
-            })
-            .then(fullEvaluation => {
-              return fullEvaluation
-            })
-            .catch(error => next(error)) 
-        })
-  
-        Promise.all(evaluationArray)
-        .then( results => {
-          //map over evaluationArray and gets questions id 
-          const repeatedQuestion = results.map( result => {
-
-            // return result[0].dataValues.question.key
-            return result[0].dataValues.question.key[1]
-            })
-            // console.log('repeatedQuestion:', repeatedQuestion)
-          //make sure there is no repeating questions
-          const distinctQuestions = [...new Set(repeatedQuestion.sort().reverse())]
-
-            // console.log('distinctQuestions:', distinctQuestions)
-
-          //map over questions id and for each question filter only the passed
-          const passedPerQuestion = distinctQuestions.map(distinctQuestionKey => {
-            const filtered = results.filter(evaluation =>  {
-            // return evaluation[0].dataValues.question.key === distinctQuestionKey
-            return evaluation[0].dataValues.question.key[1] === distinctQuestionKey
-              && 
-              evaluation[0].dataValues.passed === true
-            })
-            //return an object with qustion id and number of students who passed
-            return { questionKey: distinctQuestionKey,
-                    studentsPassed: filtered.length}
-          })
-           return res.send({ passedPerQuestion })
-        })
-        .catch(error => next(error))   
-      })
-        .catch(error => next(error))
-    })
-
-    router.get('/evaluations-by-student', (req, res, next) => {
-      Evaluation
-      .findAll({attributes:[['studentId','studentId'], ['questionId','questionId']],
-        group: ['studentId','questionId'],
-        order:[['studentId', 'ASC'],['questionId', 'ASC'],],
-       })
-       .then(evaluations => {
-          const evaluationArray =  evaluations.map(evaluation => {
-            return Evaluation
-              .findAll({
-                include: [ {model: Student},
-                  {model: Question, include: [Exercise]} 
-                 ],
-                limit: 1,
-                where: {
-                  studentId: evaluation.studentId,
-                  questionId: evaluation.questionId
-                },
-                order: [ [ 'createdAt', 'DESC' ]]
-              })
-              .then(fullEvaluation => {
-                return fullEvaluation
-              })
-              .catch(error => next(error)) 
-          })
-    
-          Promise.all(evaluationArray)
-          .then( results => {
-            //map over evaluationArray and gets students gitName
-            const repeatedStudents = results.map( result => {
-              return result[0].dataValues.student.gitName
-              })
-              console.log('repeatedStudents:', repeatedStudents)
-            //make sure there is no repeating students
-            const distinctStudent = [...new Set(repeatedStudents.sort())]
-  
-              console.log('distinctStudent:', distinctStudent)
-  
-            //map over students name and for each student filter only the passed
-            const passedPerStudent = distinctStudent.map(distinctStudentName => {
-              const filtered = results.filter(evaluation =>  {
-              return evaluation[0].dataValues.student.gitName === distinctStudentName
-                && 
-                evaluation[0].dataValues.passed === true
-              })
-              //return an object with student gitName and number of questions passed
-              return { studentName: distinctStudentName,
-                      questionsPassed: filtered.length}
-            })
-             return res.send({ passedPerStudent })
-          })
-          .catch(error => next(error))   
-        })
           .catch(error => next(error))
       })
 
-
-  router.get('/all-evaluations', (req, res, next) => {
-    Evaluation
-    .findAll({
-      include: [ {model: Student},
-         {model: Question, include: [Exercise]} 
-        ]
-      , order:[['createdAt', 'DESC']]
+      Promise.all(evaluationArray)
+        .then(() => {
+          return res.send({
+            evaluationArray
+          })
+        })
+        .catch(error => next(error))
     })
-      .then(evaluations => {
-        res.send({ evaluations })
+    .catch(error => next(error))
+})
+
+router.get('/evaluations-by-question', (req, res, next) => {
+  Evaluation
+    .findAll({
+      attributes: [['studentId', 'studentId'], ['questionId', 'questionId']],
+      group: ['studentId', 'questionId'],
+      order: [['studentId', 'ASC'], ['questionId', 'ASC'],],
+    })
+    .then(evaluations => {
+      const evaluationArray = evaluations.map(evaluation => {
+        return Evaluation
+          .findAll({
+            include: [{ model: Student },
+            { model: Question, include: [Exercise] }
+            ],
+            limit: 1,
+            where: {
+              studentId: evaluation.studentId,
+              questionId: evaluation.questionId
+            },
+            order: [['createdAt', 'DESC']]
+          })
+          .then(fullEvaluation => {
+            return fullEvaluation
+          })
+          .catch(error => next(error))
       })
-      .catch(error => next(error))
-  }) 
-  
-  router.get('/evaluations/:id', (req, res, next) => {
-    const id = req.params.id
-    Evaluation
+
+      Promise.all(evaluationArray)
+        .then(results => {
+          //map over evaluationArray and gets questions id 
+          const repeatedQuestion = results.map(result => {
+
+            // return result[0].dataValues.question.key
+            return result[0].dataValues.question.key[1]
+          })
+          // console.log('repeatedQuestion:', repeatedQuestion)
+          //make sure there is no repeating questions
+          const distinctQuestions = [...new Set(repeatedQuestion.sort().reverse())]
+
+          // console.log('distinctQuestions:', distinctQuestions)
+
+          //map over questions id and for each question filter only the passed
+          const passedPerQuestion = distinctQuestions.map(distinctQuestionKey => {
+            const filtered = results.filter(evaluation => {
+              // return evaluation[0].dataValues.question.key === distinctQuestionKey
+              return evaluation[0].dataValues.question.key[1] === distinctQuestionKey
+                &&
+                evaluation[0].dataValues.passed === true
+            })
+            //return an object with qustion id and number of students who passed
+            return {
+              questionKey: distinctQuestionKey,
+              studentsPassed: filtered.length
+            }
+          })
+          return res.send({ passedPerQuestion })
+        })
+        .catch(error => next(error))
+    })
+    .catch(error => next(error))
+})
+
+router.get('/evaluations-by-student', (req, res, next) => {
+  Evaluation
+    .findAll({
+      attributes: [['studentId', 'studentId'], ['questionId', 'questionId']],
+      group: ['studentId', 'questionId'],
+      order: [['studentId', 'ASC'], ['questionId', 'ASC'],],
+    })
+    .then(evaluations => {
+      const evaluationArray = evaluations.map(evaluation => {
+        return Evaluation
+          .findAll({
+            include: [{ model: Student },
+            { model: Question, include: [Exercise] }
+            ],
+            limit: 1,
+            where: {
+              studentId: evaluation.studentId,
+              questionId: evaluation.questionId
+            },
+            order: [['createdAt', 'DESC']]
+          })
+          .then(fullEvaluation => {
+            return fullEvaluation
+          })
+          .catch(error => next(error))
+      })
+
+      Promise.all(evaluationArray)
+        .then(results => {
+          //map over evaluationArray and gets students gitName
+          const repeatedStudents = results.map(result => {
+            return result[0].dataValues.student.gitName
+          })
+          console.log('repeatedStudents:', repeatedStudents)
+          //make sure there is no repeating students
+          const distinctStudent = [...new Set(repeatedStudents.sort())]
+
+          console.log('distinctStudent:', distinctStudent)
+
+          //map over students name and for each student filter only the passed
+          const passedPerStudent = distinctStudent.map(distinctStudentName => {
+            const filtered = results.filter(evaluation => {
+              return evaluation[0].dataValues.student.gitName === distinctStudentName
+                &&
+                evaluation[0].dataValues.passed === true
+            })
+            //return an object with student gitName and number of questions passed
+            return {
+              studentName: distinctStudentName,
+              questionsPassed: filtered.length
+            }
+          })
+          return res.send({ passedPerStudent })
+        })
+        .catch(error => next(error))
+    })
+    .catch(error => next(error))
+})
+
+
+router.get('/all-evaluations', (req, res, next) => {
+  Evaluation
+    .findAll({
+      include: [{ model: Student },
+      { model: Question, include: [Exercise] }
+      ]
+      , order: [['createdAt', 'DESC']]
+    })
+    .then(evaluations => {
+      res.send({ evaluations })
+    })
+    .catch(error => next(error))
+})
+
+router.get('/evaluations/:id', (req, res, next) => {
+  const id = req.params.id
+  Evaluation
     .findByPk(id)
     .then(evaluation => {
       if (!evaluation) {
@@ -184,85 +192,137 @@ router.get('/evaluations', (req, res, next) => {
       return res.send(evaluation)
     })
     .catch(error => next(error))
+})
+
+
+router.post('/evaluations', (req, res, next) => {
+  const day = req.body.day
+  const student = {
+    gitName: req.body.gitName,
+    gitEmail: req.body.gitEmail
+  }
+
+  const evaluationArray = JSON.parse(JSON.stringify(req.body.evaluation))
+
+  const exercisesArray = evaluationArray.map(question => {
+    return question.exercise
   })
-  
-  
-  router.post('/evaluations', (req, res, next) => {
-    const day = req.body.day
-    const student = {
-      gitName:req.body.gitName,
-      gitEmail: req.body.gitEmail
+
+  const questionsArray = evaluationArray.map(question => {
+    return {
+      exercise: question.exercise,
+      attempted: question.attempted,
+      passed: question.passed,
+      key: question.key
     }
-    
-    const evaluationArray = JSON.parse(JSON.stringify(req.body.evaluation))
-    
-    const exercisesArray = evaluationArray.map(question => {
-      return question.exercise
-    })
-    
-    const questionsArray = evaluationArray.map(question => {
-      return {
-        exercise: question.exercise,
-        attempted:question.attempted,
-        passed: question.passed,
-        key: question.key
-      }
-    })
-    
-    const exercisesUnique = exercisesArray.filter(function(question, index){
-      return exercisesArray.indexOf(question) >= index;
-    })
-    
-    //Writing the exercise name to the db
-    const createExercise = exercisesUnique.map(exercise => {
-      return Exercise
-      .findOrCreate({ where: {name: exercise, packageVersion: day} })  
-      .then( exercise =>  {
+  })
+
+  const exercisesUnique = exercisesArray.filter(function (question, index) {
+    return exercisesArray.indexOf(question) >= index;
+  })
+
+  //Writing the exercise name to the db
+  const createExercise = exercisesUnique.map(exercise => {
+    return Exercise
+      .findOrCreate({ where: { name: exercise, packageVersion: day } })
+      .then(exercise => {
         return exercise
       })
-    })
-    
-    
-    //Writing the student info to the db
-    const createStudent = Student
-    .findOrCreate({where: {gitEmail: student.gitEmail}, defaults: {gitName: student.gitName}})
+  })
+
+
+  //Writing the student info to the db
+  const createStudent = Student
+    .findOrCreate({ where: { gitEmail: student.gitEmail }, defaults: { gitName: student.gitName } })
     .then(([student]) => {
-      return student.id 
+      return student.id
     })
     .catch(next)
-    
-    const promiseArray = [createStudent, ...createExercise ]
-    
-    Promise.all(promiseArray)
+
+  const promiseArray = [createStudent, ...createExercise]
+
+  Promise.all(promiseArray)
     .then(values => {
-      const studentId = values[0] 
+      const studentId = values[0]
       let evaluation = { studentId }
-      
+
       const createQuestion = questionsArray.map(question => {
         return Exercise
-        .findOne({ where: {name: question.exercise} })  
-        .then( exercise =>  {
+          .findOne({ where: { name: question.exercise } })
+          .then(exercise => {
 
-          Question
-          .findOrCreate({ where: { key: question.key, exerciseId: exercise.id } })
-          .then(createdQuestion => {
-            evaluation = { 
-              ...evaluation,
-              passed:question.passed,
-              attempted: question.attempted,
-              questionId: createdQuestion[0].id 
-            }
-            
-            Evaluation
-            .create(evaluation)
-            .then(newEvaluation =>  newEvaluation)
+            Question
+              .findOrCreate({ where: { key: question.key, exerciseId: exercise.id } })
+              .then(createdQuestion => {
+                evaluation = {
+                  ...evaluation,
+                  passed: question.passed,
+                  attempted: question.attempted,
+                  questionId: createdQuestion[0].id
+                }
+
+                Evaluation
+                  .create(evaluation)
+                  .then(newEvaluation => newEvaluation)
+              })
+
+            return createQuestion
           })
-          
-          return createQuestion
-        })
-        .catch(error => next(error))
+          .catch(error => next(error))
       })
     })
-  })
-  
-  module.exports = router
+})
+
+router.get('/evaluations-by-question-student', (req, res, next) => {
+  Evaluation
+    .findAll({
+      attributes: [['studentId', 'studentId'], ['questionId', 'questionId']],
+      group: ['studentId', 'questionId'],
+      order: [['studentId', 'ASC'], ['questionId', 'ASC'],],
+    })
+    .then(evaluations => {
+      const evaluationArray = evaluations.map(evaluation => {
+        return Evaluation
+          .findAll({
+            include: [{ model: Student },
+            { model: Question, include: [Exercise] }
+            ],
+            limit: 1,
+            where: {
+              studentId: evaluation.studentId,
+              questionId: evaluation.questionId
+            },
+            order: [['createdAt', 'DESC']]
+          })
+          .then(fullEvaluation => {
+            return fullEvaluation
+          })
+          .catch(error => next(error))
+      })
+
+      Promise.all(evaluationArray)
+        .then(results => {
+          const passedTotal = results.filter(evaluation => {
+            // console.log('evaluation', evaluation)
+            return evaluation[0].dataValues.passed === true
+          })
+          const questionsPassed = passedTotal.length
+          console.log('questionsPassed', questionsPassed)
+          return questionsPassed
+
+            .then(
+              Promise.all([
+                Student.count(),
+                Question.count()
+              ])
+                .then(([students, questions]) => {
+                  console.log(students, questions, questionsPassed)
+                  res.send({ students, questions, questionsPassed })
+                })
+                .catch(error => next(error))   
+        )})
+    })
+})
+
+
+module.exports = router

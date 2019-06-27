4842e801fb86cb6c77fbe9436b96a079b078e03c
kerenKi <kkinberg13@gmail.com>
2019-06-27 12:04:07 +0200

    DATABASE URL on db.js

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
 
+// Heroku DATABASE_URL: postgres://uprirxcxkbrpkr:b238047354a4f0263aefbce4feccd3a4701cb9bda07d0cad427060b330da7466@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/d98eep4745c0lc
 sequelize.sync(
 // { force:true }
 )
--- setting.js
+++
@@ -1,7 +0,0 @@
-const setting = {
-  // baseUrl: 'http://localhost:4000'
-  baseUrl: 'https://pacific-ocean-98186.herokuapp.com/'
-}
-
-
-export default setting
